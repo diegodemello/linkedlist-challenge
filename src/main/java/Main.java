9e685aa54789914ffe558d7765bf79c0364bfcc7
Diego de Mello <diegodemello027@gmail.com>
2025-05-16 15:00:05 -0300

    Finalizando desafio.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,36 @@
+import java.util.LinkedList;
+
 public class Main {
     public static void main(String[] args) {
+        LinkedList<Ninja> ninjaList = new LinkedList<>();
 
+        // 1. Adicionar ninjas na LinkedList
+        ninjaList.add(new Ninja("Naruto", 21, "Aldeia da Folha"));
+        ninjaList.add(new Ninja("Sasuke", 23, "Aldeia da Folha"));
+        ninjaList.add(new Ninja("Sakura", 23, "Aldeia da Folha"));
+
+        // 2. Listar os ninjas
+        System.out.println("Lista de ninjas: ");
+        for(Ninja ninja : ninjaList){
+            System.out.println(ninja.getName());
+        }
+        System.out.println("--------------");
+
+        // 3. Adicionar o ninja no inicio da lista
+        ninjaList.addFirst(new Ninja("Boruto", 14, "Aldeia da Folha"));
+
+        // 4. Remover o ninja no inicio da lista
+        ninjaList.remove(1);
+
+        // 5. Listar os ninjas
+        System.out.println("Lista de ninjas: ");
+        for (Ninja ninja : ninjaList){
+            System.out.println(ninja.getName());
+        }
+        System.out.println("--------------");
+
+        // 6. Procurar ninja
+        Ninja segundoNinja = ninjaList.get(2);
+        System.out.println(segundoNinja.getName());
     }
 }
